86c3c5bfaad50b31fa527b52c67a4783cc58669a
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-09 15:54:17 +0530

    Added navbar

---
+++ src/Global/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+     <nav className='border-b-2 border-black'>
+        <div className='w-full bg-white h-32 overflow-x-hidden displayFlex'>
+            <div className='border-2 border-black p-2 text-3xl tracking-wider w-52  h-20 displayFlex'>Taskify</div>
+        </div>    
+    </nav> 
+    </>
+  )
+}
