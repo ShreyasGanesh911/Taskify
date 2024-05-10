6fc1304d38faf7b7499115a4e39bd4bcab05de25
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-10 10:42:32 +0530

    Code cleanup

--- src/Components/TaskModal.tsx
+++ src/Components/TaskModal.tsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
-type Obj = {
-    title :string,
-    description:string,
-    date:string
-}
+import {Obj} from '../Types/Task'
+
 type Props = {
     setShow : React.Dispatch<React.SetStateAction<boolean>>
     task:Obj[]
 }
 export default function TaskModal({setShow,task}:Props) {
     const [form,setForm] = useState({title:"",description:""})
+    const [date,setdate] = useState(new Date().toISOString().split("T")[0])
     const handleSubmit = async(e:React.FormEvent<HTMLFormElement>)=>{
         console.log(form)
         e.preventDefault()
-        localStorage.setItem('Tasks',JSON.stringify([...task,{title:form.title,description:form.description,date:"12/12/12"}])) 
+        const key = Math.ceil(Math.random()*100000)
+        localStorage.setItem('Tasks',JSON.stringify([...task,{title:form.title,description:form.description,date:date,taskKey:key,isComplete:false}])) 
         setShow(false)
         
 }
@@ -24,18 +23,19 @@ export default function TaskModal({setShow,task}:Props) {
     }
   return (
     <>
-     <form className='fixed w-full h-full top-0 bottom-0 left-0 right-0 bg-white overflow-hidden displayFlex' onSubmit={handleSubmit}>
+     <form className='fixed w-full h-full top-0 bottom-0 left-0 right-0 bg-gray-50 overflow-hidden displayFlex' onSubmit={handleSubmit}>
         <button className='absolute top-0 right-0 mr-14 mt-5 border border-black p-2 rounded' onClick={()=>setShow(false)}>
         <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
         </button>
-            <div className='w-1/2 h-1/2 bg-pink-200 displayFlex flex-col'>
+            <div className='w-full h-1/2 lg:w-1/2 bg-white  displayFlex flex-col'>
             <label className='w-3/5 '>Title <span className='text-red-500 text-lg'>*</span></label>
-            <input type="text" className='p-3 outline-none rounded border border-black w-3/5 my-3' value={form.title}  onChange={handleChange} name='title'/>   
+            <input type="text" className='p-3 outline-none rounded border border-black w-3/5 my-3' placeholder='Title here' required value={form.title}  onChange={handleChange} name='title'/>   
             <label className='w-3/5 '>Description</label> 
-            <input type="text" className='p-3 outline-none rounded border border-black w-3/5 my-3' value={form.description} onChange={handleChange} name='description' />    
-            <input type="date" min={new Date().toISOString().split("T")[0]} value={''} />
+            <input type="text" className='p-3 outline-none rounded border border-black w-3/5 my-3' placeholder='Description here' value={form.description} onChange={handleChange} name='description' />    
+            <input type="date" min={new Date().toISOString().split("T")[0]} value={date} onChange={(e)=>setdate(e.target.value)} />
+
             <button className=' p-3 my-3 rounded  bg-yellow-300 hover:bg-yellow-400'>Add task</button>
             </div>    
             
--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
 import TaskModal from '../Components/TaskModal'
+import { ToastContainer} from 'react-toastify';
+import { toastSuccess } from '../Toast/Toast';
 
 export default function Home() {
-    const [task,setTask] = useState([{title:"",date:'',description:''}])
-    const[title,setTitle] = useState('')
+    const [task,setTask] = useState([{title:"",date:'',description:'',taskKey:0,isComplete:false}])
+    const[change,setChange] = useState(false)
     const [show,setShow] = useState(false)
-    const handleSubmit = async(e:React.FormEvent<HTMLFormElement>)=>{
-            setShow(true)
-            setTitle('')
-            e.preventDefault()
-          
-            
-    }
+  const handleClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+    e.preventDefault()
+    
+    setShow(true)
+  }
     const getData = ()=>{
         if(localStorage.getItem("Tasks") !== null){
             const data = JSON.parse(localStorage.getItem("Tasks") || "{}") 
@@ -21,11 +21,12 @@ export default function Home() {
        else{
         localStorage.setItem("Tasks",JSON.stringify([]))
         setTask([])
-       }       
+       }  
+           
     }
     useEffect(()=>{
         getData()
-    },[show])
+    },[show,change])
   return (
     <>
         {!show?
@@ -33,35 +34,35 @@ export default function Home() {
       <TaskModal setShow={setShow} task={task}/>
         }
          <section className='page bg-gray-50'>
-             <form className='w-full h-44 displayFlex' onSubmit={handleSubmit}>
-                 <button className='border border-black p-4 rounded'>Add task +</button>
+             <form className='w-full h-44 displayFlex' >
+                 <button className='border border-black p-4 rounded' onClick={handleClick}>Add task +</button>
              </form>
             {task.length>0    ? 
              
                  <div className='flex flex-wrap w-full  p-4 justify-evenly overflow-x-hidden'>
-                      {task.map((e)=>{
-                 return(<Card title={e.title} description={e.description} date={e.date}   />)
-             })}
+                        {task.filter((e)=>!e.isComplete).map((e)=>{
+                            return(<Card title={e.title} description={e.description} date={e.date} taskKey={e.taskKey} key={e.taskKey} isComplete={e.isComplete} show={change} setShow={setChange} />)
+                        })}
+                         {task.filter((e)=>e.isComplete).map((e)=>{
+                            return(<Card title={e.title} description={e.description} date={e.date} taskKey={e.taskKey} key={e.taskKey} isComplete={e.isComplete} show={change} setShow={setChange} />)
+                        })}
                  </div>
              :
-             <div className='w-full text-center p-4 text-6xl'>
+             <div className='w-full text-center p-4 text-6xl font-mono'>
                  No task
+                 <p className='text-lg p-1 my-2'>Try adding a task</p>
              </div>
          }
- 
+            <ToastContainer/>
       </section>
+      
     </>
   )
 }
 
-/*
-    <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+/**
+                          {task.map((e)=>{
+                 return(<Card title={e.title} description={e.description} date={e.date} taskKey={e.taskKey} key={e.taskKey} isComplete={e.isComplete} show={change} setShow={setChange} />)
+             })}
 
-*/
+ */
