777dcdf31c15e7749b4f06fe8c38f1c8f0b9d841
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-10 10:50:03 +0530

    Footer changes

--- src/Global/Footer.tsx
+++ src/Global/Footer.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-     <div className='w-full h-72 displayFlex'>
-        <div className='w-1/2 h-full bg-pink-200 border-r-2 border-black displayFlex flex-col'>
+     <div className='w-full h-72 displayFlex flex-col bg-amber-300'>
+        <div className='w-full h-full   displayFlex flex-col'>
         <div className='border-2 border-black p-2 text-3xl tracking-wider w-52  h-20 displayFlex'>Taskify</div>
-        <p>Task made easy</p>
+        <p className='my-2'>Task made easy</p>
         </div>
-        <div className='w-1/2 h-full bg-pink-200'></div>
+       <p className='relative bottom-0 left-0 text-black '>Made with ❤️ by shreyas</p>
      </div> 
     </>
   )
